20262fcbf42b4c730c25b0cdd604454206c27944
RavinduThilinaka <ravindu2001thilinaka@gmail.com>
2025-02-04 22:49:25 +0530

    add to jsx file

---
+++ starlight/src/components/Bannner/BannerText.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BannerText() {
+  return (
+    <div>BannerText</div>
+  )
+}
+
+export default BannerText
--- starlight/tailwind.config.js
+++ starlight/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html","./src/**/*.{js,ts,jsx,tsx}"],
+    
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#aa6d6b",
+        brandDark:"#151616",
+      },
+ 
+      container:{
+        center:true,
+        padding:{
+          DEFAULT:"1rem",
+          sm:"2rem",
+          lg:"4rem",
+          xl:"5rem",
+          "2xl":"6rem",
+        }
+      }
+    },
+    
   },
   plugins: [],
 }
