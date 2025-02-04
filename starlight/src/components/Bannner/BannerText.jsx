66a157c1ee52492c26324a2673595b53e23a72f3
RavinduThilinaka <ravindu2001thilinaka@gmail.com>
2025-02-05 01:00:17 +0530

    add services ,Banner ,Blog and footer page

--- starlight/src/components/Bannner/BannerText.jsx
+++ starlight/src/components/Bannner/BannerText.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { UpdateFollower } from 'react-mouse-follower'
 
 function BannerText() {
   return (
-    <div>BannerText</div>
+    <>
+        <section className='py-12 text-center'>
+            <div className="container">
+                <div className="bg-gradient-to-t from bg-primary to to-primary/70 text-white rounded-3xl p-8 hover:scale-105 duration-500 hover:shadow-2xl">
+                    <UpdateFollower mouseOptions={{backgroundColor:"black", zIndex:9999,followSpeed:0.5, mixBlendMode:"screen", scale:10}}>
+                        <p className='font-bold text-4xl max-w-[700px] mx-auto leading-normal'>Headphones with good Qulity And Affordable Price</p>
+                    </UpdateFollower>
+                </div>
+            </div>
+        </section>
+    </>
   )
 }
 
