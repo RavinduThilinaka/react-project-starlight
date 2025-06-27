2441142544d4cdb7673361e6542d4fea1b3e83d3
RavinduThilinaka <ravindu2001thilinaka@gmail.com>
2025-06-27 05:55:05 +0530

    complte payment crude oparation

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const cookieParser = require('cookie-parser');
 
 app.use(cors({
     origin:"http://localhost:5173",
-    methods:["GET","POST","PATCH","DELETE"],
+    methods:["GET","POST","PUT","PATCH","DELETE"],
     credentials:true,
 }));
 
